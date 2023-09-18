f13b56f36f847c1c02ec657be5535f1bff95646c
Matheus H Ramos <matheus.hrs@live.com>
2023-09-17 21:01:20 -0300

    Add last page control

--- src/main/java/br/com/matheushramos/batch/job/reader/OrderFoodReader.java
+++ src/main/java/br/com/matheushramos/batch/job/reader/OrderFoodReader.java
@@ -2,33 +2,42 @@ package br.com.matheushramos.batch.job.reader;
 
 import br.com.matheushramos.batch.enums.StatusEnum;
 import br.com.matheushramos.batch.model.Order;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.data.MongoItemReader;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Component;
 
 import java.util.Iterator;
 import java.util.List;
 
 @Slf4j
-@Setter
+@Component
 public class OrderFoodReader extends MongoItemReader<Order> {
 
+    public static final Integer PAGE_SIZE = 5;
+
     MongoTemplate mongoTemplate;
+
+    private boolean lastPage;
 
     public OrderFoodReader(MongoTemplate mongoTemplate) {
         this.mongoTemplate = mongoTemplate;
         setTemplate(mongoTemplate);
-        setPageSize(5);
+        setPageSize(PAGE_SIZE);
         setSaveState(false);
         setTargetType(Order.class);
     }
 
     @Override
     public Iterator<Order> doPageRead() {
+        if (this.lastPage) {
+            log.info("Last page executed. No more records to read.");
+            return null;
+        }
+
         final var pageable = PageRequest.of(this.page, this.pageSize);
         final var skipValue = (long) pageable.getPageNumber() * (long) pageable.getPageSize();
 
@@ -42,6 +51,10 @@ public class OrderFoodReader extends MongoItemReader<Order> {
         List<Order> orders = this.mongoTemplate.find(q, Order.class);
 
         log.info("Read orders. Total: {}, Page: {}", orders.size(), pageable.getPageNumber());
+
+        if (orders.size() < PAGE_SIZE.doubleValue()) {
+            this.lastPage = true;
+        }
 
         return orders.iterator();
     }
